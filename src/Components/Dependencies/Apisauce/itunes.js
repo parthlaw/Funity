51bb14a552a2ca8db1df21d65f7ae4ea3888a8a4
Parth <img_2019045@iiitm.ac.in>
2020-09-28 18:33:36 +0530

    access control allow origin header

--- src/Components/Dependencies/Apisauce/itunes.js
+++ src/Components/Dependencies/Apisauce/itunes.js
@@ -2,6 +2,9 @@ import { create } from "apisauce";
 
 const api = create({
   baseURL: "https://itunes.apple.com",
+  headers: {
+    "Access-Control-Allow-Origin": "https://funity.vercel.app/",
+  },
 });
 const callAPI = async (searchString) => {
   const data = await api.get(`/search?term=${searchString}`);
